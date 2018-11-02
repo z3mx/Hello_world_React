ce0222d4f903c56f65a4d03a3cdc7fe606d6ce5e
Luis B <z3mx@users.noreply.github.com>
2018-11-01 20:36:07 -0600

    Add files via upload

---
+++ src/components/Global/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <h1>Soy el Contenido</h1>
+        <p>lorem impsum...</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
